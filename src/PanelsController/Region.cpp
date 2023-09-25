39b975b5082ed4ab452f9f120c939b08204a4ab6
Peter Polidoro <peter@polidoro.io>
2023-09-25 08:55:59 -0400

    Replace accidentally deleted Region::setup

--- src/PanelsController/Region.cpp
+++ src/PanelsController/Region.cpp
@@ -9,6 +9,24 @@
 
 using namespace panels_controller;
 
+void Region::setup(SPIClass * spi_ptr)
+{
+  spi_ptr_ = spi_ptr;
+  spi_ptr_->begin();
+
+  for (uint8_t byte_index = 0; byte_index<constants::BYTE_COUNT_PER_PANEL_GRAYSCALE; ++byte_index)
+  {
+    if ((byte_index == 0) || (byte_index == 33) || (byte_index == 66) || (byte_index == 99))
+    {
+      output_buffer_[byte_index] = 1;
+    }
+    else
+    {
+      output_buffer_[byte_index] = 255;
+    }
+  }
+}
+
 void Region::beginTransferPanel(SPISettings spi_settings)
 {
   spi_ptr_->beginTransaction(spi_settings);
